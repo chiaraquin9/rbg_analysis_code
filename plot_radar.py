ce0dccba00e19925698d203bad2c20c11e07991b
Chiara Aquino <s1326314@burn.geos.ed.ac.uk>
2018-10-23 16:40:01 +0100

    radar analysis

--- plot_radar.py
+++ plot_radar.py
@@ -14,8 +14,8 @@ import numpy as np
 from matplotlib.lines import Line2D
 import os, os.path
 
-dirpath = '/exports/csce/datastore/geos/users/s1326314/rbg_analysis'
-#dirpath = 'R:/rbg_analysis'
+#dirpath = '/exports/csce/datastore/geos/users/s1326314/rbg_analysis'
+dirpath = 'R:/rbg_analysis'
 
 #plotting parameters
 params = {'legend.fontsize': 'x-large',
@@ -54,7 +54,7 @@ matches.sort(key=lambda x: float(x[0]))
 
 #get values for AGB, HV, dHV, HH, dHH,HH/HV, dHH/HV depending from year of choice
 
-y = '07' #set year of choice
+y = '15' #set year of choice
 
 plot_no = [float(i[1]) for i in matches if i[0] == y]
 AGB = [float(i[2]) for i in matches if i[0] == y]
@@ -66,71 +66,108 @@ HV_HH = [float(i[7]) for i in matches if i[0] == y]
 dHV_HH = [float(i[8]) for i in matches if i[0] == y]
 
 
-# select red for logged and green for unlogged plots
-colors = ['red' if i > 11 else 'green' for i in plot_no]
-lines = [Line2D([0], [0], marker='o', color='r', lw=3, label='Logged Plots'),
-                   Line2D([0], [0], marker='o', color='g', lw=3, label='Unlogged Plots')]
-
-fig = plt.figure(1, figsize=(9,9))
-
-ax1 = fig.add_subplot(2,2,1)
-for pos,y,err,color in zip(AGB,HV,dHV,colors):
-    ax1.errorbar(pos,y,err,lw=2,capsize=5,fmt='o',capthick=2,color=color)
-    plt.title('HV vs AGB')
-    plt.xlabel('AGB [Mg/ha]')
-    plt.ylabel('HV [db]')
-    plt.ylim(-16,-9)
-    
-ax2 = fig.add_subplot(2,2,2)
-for pos,y,err,color in zip(AGB,HH,dHH,colors):
-    ax2.errorbar(pos,y,err,lw=2,capsize=5,fmt='o',capthick=2,color=color)
-    plt.title('HH vs AGB')
-    plt.xlabel('AGB [Mg/ha]')
-    plt.ylabel('HH [db]')
-    plt.ylim(-11,-3)
-    
-ax3 = fig.add_subplot(2,1,2)
-for pos,y,err,color in zip(AGB,HV_HH,dHV_HH,colors):
-    ax3.errorbar(pos,y,err,lw=2,capsize=5,fmt='o',capthick=2,color=color)
-    plt.title('HV/HH vs AGB')
-    plt.xlabel('AGB [Mg/ha]')
-    plt.ylabel('HV/HH')
-    plt.ylim(0,1)
-    
-
-plt.figlegend(handles=lines,loc='lower left')
-plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-fig.suptitle('ALOS/PALSAR Backscatter vs AGB per plot for year 2007',fontsize=18)
-plt.show()
-fig.savefig(dirpath + '/outputs/site1/radar/ALOS_PALSAR/sigma0_AGB_07.pdf', bbox_inches='tight')
+## select red for logged and green for unlogged plots
+#colors = ['red' if i > 11 else 'green' for i in plot_no]
+#lines = [Line2D([0], [0], marker='o', color='r', lw=3, label='Logged Plots'),
+#                   Line2D([0], [0], marker='o', color='g', lw=3, label='Unlogged Plots')]
+#
+#fig = plt.figure(1, figsize=(9,9))
+#
+#ax1 = fig.add_subplot(2,2,1)
+#for pos,y,err,color in zip(AGB,HV,dHV,colors):
+#    ax1.errorbar(pos,y,err,lw=2,capsize=5,fmt='o',capthick=2,color=color)
+#    plt.title('HV vs AGB')
+#    plt.xlabel('AGB [Mg/ha]')
+#    plt.ylabel('HV [db]')
+#    plt.ylim(-16,-9)
+#    
+#ax2 = fig.add_subplot(2,2,2)
+#for pos,y,err,color in zip(AGB,HH,dHH,colors):
+#    ax2.errorbar(pos,y,err,lw=2,capsize=5,fmt='o',capthick=2,color=color)
+#    plt.title('HH vs AGB')
+#    plt.xlabel('AGB [Mg/ha]')
+#    plt.ylabel('HH [db]')
+#    plt.ylim(-11,-3)
+#    
+#ax3 = fig.add_subplot(2,1,2)
+#for pos,y,err,color in zip(AGB,HV_HH,dHV_HH,colors):
+#    ax3.errorbar(pos,y,err,lw=2,capsize=5,fmt='o',capthick=2,color=color)
+#    plt.title('HV/HH vs AGB')
+#    plt.xlabel('AGB [Mg/ha]')
+#    plt.ylabel('HV/HH')
+#    plt.ylim(0,1)
+#    
+#
+#plt.figlegend(handles=lines,loc='lower left')
+#plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+#fig.suptitle('ALOS/PALSAR Backscatter vs AGB per plot for year 2007',fontsize=18)
+#plt.show()
+#fig.savefig(dirpath + '/outputs/site1/radar/ALOS_PALSAR/sigma0_AGB_07.pdf', bbox_inches='tight')
 
 # plot temporal variation 
 
 y2 = '17' #set year of choice
 
-#plot_no17 = [float(i[1]) for i in matches if i[0] == y2]
-AGB17 = [float(i[2]) for i in matches if i[0] == y2]
-#HV17 = [float(i[3]) for i in matches if i[0] == y2]
+HV17 = [float(i[3]) for i in matches if i[0] == y2]
 #dHV17 = [float(i[4]) for i in matches if i[0] == y2]
 #HH17 = [float(i[5]) for i in matches if i[0] == y2]
 #dHH17 = [float(i[6]) for i in matches if i[0] == y2]
 HV_HH17 = [float(i[7]) for i in matches if i[0] == y2]
 dHV_HH17 = [float(i[8]) for i in matches if i[0] == y2]
 
-lines_time=[Line2D([0], [0], marker='o', color='r', mfc='white', lw=1, label='Logged Plots 2007'),
-            Line2D([0], [0], marker='o', color='g', mfc='white', lw=1, label='Unlogged Plots 2007'),
-            Line2D([0], [0], marker='o', color='r', lw=2, label='Logged Plots 2017'),
-                   Line2D([0], [0], marker='o', color='g', lw=2, label='Unlogged Plots 2017')]
+#lines_time=[Line2D([0], [0], marker='o', color='r', mfc='white', lw=1, label='Logged Plots 2007'),
+#            Line2D([0], [0], marker='o', color='g', mfc='white', lw=1, label='Unlogged Plots 2007'),
+#            Line2D([0], [0], marker='o', color='r', lw=2, label='Logged Plots 2017'),
+#                   Line2D([0], [0], marker='o', color='g', lw=2, label='Unlogged Plots 2017')]
+#
+#fig = plt.figure(2, figsize=(9,9))
+#for pos,y,err,color in zip(AGB,HV_HH,dHV_HH,colors):
+#    plt.errorbar(pos,y,err,lw=2,capsize=5,capthick=1,mfc='white', color=color,fmt='o')
+#for pos,y,err,color in zip(AGB17,HV_HH17,dHV_HH17,colors):
+#    plt.errorbar(pos,y,err,lw=2,capsize=5,capthick=2,color=color,fmt='o')
+#    plt.ylim(0,1)
+#    plt.xlabel('AGB [Mg/ha]')
+#    plt.ylabel('HV/HH')
+#    plt.title('ALOS/PALSAR temporal variation in HH/HV vs AGB per plot for years 2007 and 2017',fontsize=18, pad=30)
+#    plt.figlegend(handles=lines_time,loc=(0.2,0.1))
+#    fig.savefig(dirpath + '/outputs/site1/radar/ALOS_PALSAR/HH_HV_compare07_17.pdf', bbox_inches='tight')
 
-fig = plt.figure(2, figsize=(9,9))
-for pos,y,err,color in zip(AGB,HV_HH,dHV_HH,colors):
-    plt.errorbar(pos,y,err,lw=2,capsize=5,capthick=1,mfc='white', color=color,fmt='o')
-for pos,y,err,color in zip(AGB17,HV_HH17,dHV_HH17,colors):
-    plt.errorbar(pos,y,err,lw=2,capsize=5,capthick=2,color=color,fmt='o')
-    plt.ylim(0,1)
-    plt.xlabel('AGB [Mg/ha]')
-    plt.ylabel('HV/HH')
-    plt.title('ALOS/PALSAR temporal variation in HH/HV vs AGB per plot for years 2007 and 2017',fontsize=18, pad=30)
-    plt.figlegend(handles=lines_time,loc=(0.2,0.1))
-    fig.savefig(dirpath + '/outputs/site1/radar/ALOS_PALSAR/HH_HV_compare07_17.pdf', bbox_inches='tight')
 
+# plot temporal variation
+
+lines = [Line2D([0], [1], color='r', lw=3, label='Logged Plots'),
+                   Line2D([0], [1], marker='o', color='g', lw=3, label='Unlogged Plots')]
+
+width = 0.3   # the width of the bars
+w = [i+width for i in plot_no]  # the position of the second bar     
+
+fig = plt.figure(3)
+ax = fig.add_subplot(111)
+ax.set_axisbelow(True)
+ax.yaxis.grid(color='gray', linestyle='dashed')
+rects1 = ax.bar(plot_no, HV, width, color=['turquoise' if i < 12 else 'teal' for i in plot_no],edgecolor='black')
+rects2 = ax.bar(w, HV17, width, color=['firebrick' if i < 12 else 'maroon' for i in plot_no],edgecolor='black')
+# add some
+ax.set_ylabel('Backscatter coefficient [dB]')
+ax.set_ylim(-16,0)
+ax.set_xlabel('Plot number')
+ax.set_title('ALOS PALSAS HV-polarized backscatter measurements',fontsize=14, pad=15)
+ax.set_xticks([(x + y)/2 for x, y in zip(plot_no, w)])
+ax.set_xticklabels([i for i in range(1,17)])
+
+
+#ax.annotate('', xy=(12, -15), xytext=(16.5, -15),
+#            arrowprops={'arrowstyle': '<->'})
+ax.annotate('Logged', xy=(14,-13.5), ha='center', va='center',fontsize=13, weight='bold')
+#ax.annotate('', xy=(0.7, -15), xytext=(11.5, -15),
+#            arrowprops={'arrowstyle': '<->'})
+ax.annotate('Unlogged', xy=(6,-13.5), ha='center', va='center',fontsize=13,weight='bold')
+
+first = plt.legend((rects1[0], rects2[0]), ('2015', '2017'),
+          ncol=1,loc='lower left')
+ax = plt.gca().add_artist(first)
+plt.legend( (rects1[12], rects2[12]), ('2015', '2017'),
+          ncol=1,loc='lower right')
+fig.savefig(dirpath + '/outputs/site1/radar/ALOS_PALSAR/temporal_variation15_17.pdf', bbox_inches='tight')
+
+plt.show()
